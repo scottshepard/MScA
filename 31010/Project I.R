164d1d75a03429c0497ee9ded66545b12247d8bc
Scott Shepard <scott@shepardmail.com>
2019-03-07 18:21:31 -0600

    31010: Final Project (#28)
    * Quiz I, Part I
    
    * Start of Part II
    
    * Part 1, Quiz 2
    
    * Start of step 3
    
    * Completed step 3
    
    * Write results of section 1
    
    * Steps 4 & 5 of Part II
    
    * Finished last part of the project

---
+++ 31010/Project I.R
@@ -0,0 +1,175 @@
+library(dplyr)
+library(MASS)
+library(pscl)
+
+dataPath <- "~/Dropbox/MScA/31010 - Linear Non Linear/Course Project/"
+Course.Project.Data <- read.csv(file=paste(dataPath,"MScA_31010_LinearNonLinear_MalfunctionData.csv",sep="/"))
+head(Course.Project.Data,10)
+
+# Step 1
+
+counting_process<-as.data.frame(cbind(Time=Course.Project.Data$Time,                                      
+                                      Count=1:length(Course.Project.Data$Time)))
+counting_process[1:10,]
+
+plot(counting_process$Time,counting_process$Count,type="s")
+
+plot(counting_process$Time,
+     counting_process$Count/counting_process$Time,type="l",
+     ylab="Cumulative Intensity")
+abline(h=counting_process$Count[length(counting_process$Count)]/
+         counting_process$Time[length(counting_process$Time)])
+abline(h=mean(counting_process$Count/counting_process$Time))
+
+c(Last.Intensity=counting_process$Count[length(counting_process$Count)]/
+    counting_process$Time[length(counting_process$Time)],
+  Mean.Intensity=mean(counting_process$Count/counting_process$Time))
+
+Course.Project.Data$minute <- floor(Course.Project.Data$Time / 60) + 1
+
+minute_data <- Course.Project.Data %>% 
+  group_by(minute) %>%
+  summarize( count=n(), temperature = mean(Temperature)) %>%
+  data.frame()
+
+head(minute_data, 2)
+dim(minute_data)
+
+# Step 1 Quiz 
+minute_data[67,]
+
+# Step 2
+# Analysis of overdispersion
+# Use three different methods to analyze dispersion
+m1 <- glm(count ~ minute, data=minute_data, family=poisson())
+
+# Method 1 - CI based on deviance & dof
+ci_chisq <- function(model) {
+  deviance = summary(model)$deviance
+  dof = summary(model)$df.residual
+  qchisq(c(.025,.975), df=dof, lower.tail=FALSE)
+}
+summary(m1)$deviance
+ci_chisq(m1)
+# The deviance far outside the confidence interval for Chi-Squared
+
+# Method 2 - Regression Test
+AER::dispersiontest(m1)
+# The dispersion test shows the same thing the CI test did, p-value is small
+# enought to reject the null. True dispersion is greater than 1.
+
+# Method 3 - Test Against Neg Binom
+m1.nb <- MASS::glm.nb(count ~ minute, data=minute_data)
+pscl::odTest(m1.nb)
+# Again, p-value is very small. Data cannot be described by Poisson.
+
+
+# Step 2 Quiz
+# Repeat three over-dispersion tests above on new data
+test_dat <- read.table(paste(dataPath,'MScA_LinearNonlinear_step2_sample.csv',sep = '/'), header=TRUE)
+test_dat$minute <- as.numeric(row.names(test_dat))
+
+m2 <- glm(count ~ minute, data=test_dat, family=poisson())
+summary(m2)$deviance
+ci_chisq(m2)
+# Deviance: 578.6287
+# Lower Limit: 438.0601
+# Upper Limit: 561.7274
+
+AER::dispersiontest(m2)
+# test stat: 2.0338
+# p-value: 0.02099
+
+m2.nb <- MASS::glm.nb(count ~ minute, data=test_dat)
+pscl::odTest(m2.nb)
+# test stat: 4.7795
+# p-value: 0.0144
+
+# Step 3
+# Analyses of distributions of variables
+library(lattice)
+library(latticeExtra)
+
+sample1=rnorm(100)
+sample2=rnorm(100,1,2)
+Cum.Distr.Functions <- data.frame(sample1,sample2)
+ecdfplot(~ sample1 + sample2, data=Cum.Distr.Functions, auto.key=list(space='right'))
+
+ks.test(sample1,sample2)
+
+ks.test(sample1,"pnorm",mean=0,sd=1)
+
+ks.test(sample2,"pnorm",mean=0,sd=1)
+
+counting_process$Time
+
+fitExp <- MASS::fitdistr(counting_process$Time, "exponential")
+KSTest_event_intervals <- ks.test(counting_process$Time, "pexp", rate=fitExp$estimate)
+c(KSTest_event_intervals$statistic,p.value=KSTest_event_intervals$p.value)
+
+plot(ecdf(diff(counting_process$Time)),
+     ylab="CDF",xlab="Time Intervals", 
+     main="ECDF")
+
+# Fit 5 candidates for distribution to event_indensities
+event_intensities = minute_data$count
+hist(event_intensities)
+
+# Normal
+fit.norm = MASS::fitdistr(event_intensities, densfun='normal')
+ks.test(event_intensities,"pnorm",mean=fit.norm$estimate[1],sd=fit.norm$estimate[2])
+# D = 0.15707, p-value = 8.78e-06
+
+# Exponential
+fit.exp = MASS::fitdistr(event_intensities, densfun = "exponential")
+ks.test(event_intensities, "pexp", rate = fit.exp$estimate)
+# D = 0.17956, p-value = 1.996e-07
+
+# Gamma
+fit.gam = MASS::fitdistr(event_intensities, densfun="gamma")
+ks.test(event_intensities, "pgamma", shape=fit.gam$estimate[1], rate=fit.gam$estimate[2])
+# D = 0.076201, p-value = 0.1097
+
+# Log Normal
+fit.lognorm = MASS::fitdistr(event_intensities, densfun = "lognormal")
+ks.test(event_intensities, "plnorm", meanlog=fit.lognorm$estimate[1], sdlog=fit.lognorm$estimate[2])
+# D = 0.12003, p-value = 0.001488
+
+# Logistic
+fit.logit = MASS::fitdistr(event_intensities, densfun="logistic")
+ks.test(event_intensities, "plogis", location=fit.logit$estimate[1], scale=fit.logit$estimate[2])
+# D = 0.10735, p-value = 0.006287
+
+# Fit the same five candidates on temperature
+temps = minute_data$temperature
+
+# Normal
+fit.norm = MASS::fitdistr(temps, densfun='normal')
+ks.test(temps,"pnorm",mean=fit.norm$estimate[1],sd=fit.norm$estimate[2])
+# D = 0.041953, p-value = 0.7711
+
+# Exponential
+fit.exp = MASS::fitdistr(temps, densfun = "exponential")
+ks.test(temps, "pexp", rate = fit.exp$estimate)
+# D = 0.57975, p-value < 2.2e-16
+
+# Gamma
+fit.gam = MASS::fitdistr(temps, densfun="gamma")
+ks.test(temps, "pgamma", shape=fit.gam$estimate[1], rate=fit.gam$estimate[2])
+# D = 0.048746, p-value = 0.5924
+
+# Log Normal
+fit.lognorm = MASS::fitdistr(temps, densfun = "lognormal")
+ks.test(temps, "plnorm", meanlog=fit.lognorm$estimate[1], sdlog=fit.lognorm$estimate[2])
+# D = 0.052065, p-value = 0.5069
+
+# Logistic
+fit.logit = MASS::fitdistr(temps, densfun="logistic")
+ks.test(temps, "plogis", location=fit.logit$estimate[1], scale=fit.logit$estimate[2])
+# D = 0.038929, p-value = 0.8431
+
+fit.intensity.gam = MASS::fitdistr(event_intensities, densfun="gamma")
+fit.temp.norm = MASS::fitdistr(temps, densfun='normal')
+distrParam <- list(intensity=fit.intensity.gam, temperature=fit.temp.norm)
+
+write.csv(minute_data,file=paste(dataPath,"OneMinuteCountsTemps.csv",sep="/"),row.names=FALSE)
---
+++ 31010/Project II.R
@@ -0,0 +1,195 @@
+library(copula)
+
+part2_data<-read.csv(file=paste(dataPath,"OneMinuteCountsTemps.csv",sep="/"))
+head(part2_data)
+
+part2_data<-part2_data[complete.cases(part2_data),]
+
+part2_data$intensity<-part2_data[,"count"]
+head(part2_data)
+
+plot(part2_data$temperature,part2_data$intensity)
+
+plot(rank(part2_data$temperature)/(dim(part2_data)[1]+1),
+     rank(part2_data$intensity)/(dim(part2_data)[1]+1),xlab="Temperature",ylab="Intensity")
+
+plot(copula::pobs(part2_data[,c("temperature","intensity")]),xlab="Temperature",ylab="Intensity")
+
+
+# Part 1 - Fit Parametric Copulas
+# Normal, Gumbel, Clayton, Frank
+
+copula_data <- copula::pobs(part2_data[,c("temperature","intensity")])
+
+fit.copula.normal <- fitCopula(
+  copula = normalCopula(),
+  data = copula_data,
+  method = "ml",
+  optim.method = "BFGS")
+fit.copula.normal@loglik
+# 198.146
+
+fit.copula.gumbel <- fitCopula(
+  copula = gumbelCopula(),
+  data = copula_data,
+  method = "ml",
+  optim.method = "BFGS")
+fit.copula.gumbel@loglik
+# 247.092
+
+fit.copula.clayton <- fitCopula(
+  copula = claytonCopula(),
+  data = 1-copula_data,
+  method = "ml",
+  optim.method = "BFGS")
+fit.copula.clayton@loglik
+# 111.659 -- wrong for some reason
+# 259.973
+
+fit.copula.frank <- fitCopula(
+  copula = frankCopula(),
+  data = copula_data,
+  method = "ml",
+  optim.method = "BFGS")
+fit.copula.frank@loglik
+# 211.514
+
+# Part 2 - Copula quantile regression
+
+alphaClayton = summary(fit.copula.clayton)$coefficients[1]
+copulaClayton_sim <- claytonCopula(param=alphaClayton,dim=2)
+persp(copulaClayton_sim, dCopula, main="pdf",xlab="u", ylab="v", zlab="c(u,v)")
+
+contour(copulaClayton_sim,dCopula, main="pdf",xlab="u", ylab="v")
+
+simData<-rCopula(1000,copulaClayton_sim)
+plot(simData)
+
+contour(copula_data)
+
+distrParam
+
+# Follow the steps of copula quantile regression:
+
+# 1. Calculate copula image of the predictor imgX using the marginal 
+#    distribution fitted to the data in Part 1
+temp_mean = distrParam$temperature$estimate[1]
+temp_sd = distrParam$temperature$estimate[2]
+
+imgX <- pnorm(part2_data$temperature, mean=temp_mean, sd=temp_sd)
+
+# 2. If the copula needs to be inverted from upper tail to lower tail or 
+#    vice versa, rotate the image by imgX <- -imgX+1
+copulat_data_rot = 1-copula_data
+imgX <- 1-imgX
+
+# 3. Calculate image quantiles of conditional distribution V|U corresponding 
+#    to the selected copula type, its parameter and every point of
+#    imgX (image of X) for levels 5%, 95% and 50% (median). Use formulas given
+#    in the lecture for quantiles of conditional distributions corresponding to 
+#    different copula types
+
+qclayton <- function(X, alpha_, theta_) {
+  # Clayton conditional distribution quantile
+  ((alpha_**(-theta_/(1+theta_)) - 1) * X**(-theta_) + 1) ** (-1/theta_)
+}
+
+theta <- fit.copula.clayton@estimate
+
+intensity.Q.Low  <- qclayton(imgX, alpha_=0.95, theta_=theta)
+intensity.Q.Mid  <- qclayton(imgX, alpha_=0.50, theta_=theta)
+intensity.Q.High <- qclayton(imgX, alpha_=0.05, theta_=theta)
+
+plot(cbind(imgX, copulat_data_rot[,2]))
+points(imgX,intensity.Q.Low,col="orange",pch=16)
+points(imgX,intensity.Q.Mid,col="cyan",pch=16)
+points(imgX,intensity.Q.High,col="orange")
+
+# 4. Reconstruct quantiles for the original data units from the image quantiles
+#   using marginal distributions fitted to the variables. Remember to rotate 
+#   image before reconstructing if necessary
+
+intensity.Q.Low  = 1 - intensity.Q.Low
+intensity.Q.Mid  = 1 - intensity.Q.Mid
+intensity.Q.High = 1 - intensity.Q.High
+
+fitted_shape = distrParam$intensity$estimate[1]
+fitted_rate  = distrParam$intensity$estimate[2]
+
+intensLow  = qgamma(intensity.Q.Low,  shape=fitted_shape, rate=fitted_rate)
+intensMid  = qgamma(intensity.Q.Mid,  shape=fitted_shape, rate=fitted_rate)
+intensHigh = qgamma(intensity.Q.High, shape=fitted_shape, rate=fitted_rate)
+
+# 5. Use reconstructed quantiles as predictions of conditional quantiles of 
+#    Y given X. In particular, predict Y by the 50% quantile (median)
+
+lowMidHigh = data.frame(
+  "intensLow" = intensLow,  
+  "intensMid" = intensMid, 
+  "intensHigh" = intensHigh
+  )
+
+df <- cbind(part2_data, lowMidHigh)
+anomalies <- df[df$intensity > df$intensHigh,]
+
+plot(part2_data$temperature,part2_data$intensity)
+points(part2_data$temperature,lowMidHigh[,"intensLow"],col="orange")
+points(part2_data$temperature,lowMidHigh[,"intensHigh"],col="red")
+points(part2_data$temperature,lowMidHigh[,"intensMid"],col="cyan")
+points(part2_data$temperature[anomalies$minute],
+       part2_data$intensity[anomalies$minute],col="magenta",pch=16)
+
+
+# Step 5 Test
+
+dat <- readRDS(paste(dataPath,'nonlinear_models_course_assignment_step_5_data.rds',sep = '/'))
+
+test_copula_data = pobs(cbind(dat$predictor, dat$output))
+plot(test_copula_data)
+
+test_imgX <- pnorm(dat$predictor, mean=dat$predictor_DistrParameters[1], sd=dat$predictor_DistrParameters[2])
+test_imgX_rot <- 1-test_imgX
+
+fit.copula.clayton.test <- fitCopula(
+  copula = claytonCopula(),
+  data = 1-test_copula_data,
+  method = "ml",
+  optim.method = "BFGS")
+fit.copula.clayton.test@loglik
+
+theta <- fit.copula.clayton.test@estimate
+test.output.Q.Low  <- 1-qclayton(test_imgX_rot, alpha_=0.95, theta_=theta)
+test.output.Q.Mid  <- 1-qclayton(test_imgX_rot, alpha_=0.50, theta_=theta)
+test.output.Q.High <- 1-qclayton(test_imgX_rot, alpha_=0.05, theta_=theta)
+
+test_fitted_mean = dat$output_DistrParameters[1]
+test_fitted_sd = dat$output_DistrParameters[2]
+
+test.output.Low  = qlnorm(test.output.Q.Low,  meanlog=test_fitted_mean, sdlog=test_fitted_sd)
+test.output.Mid  = qlnorm(test.output.Q.Mid,  meanlog=test_fitted_mean, sdlog=test_fitted_sd)
+test.output.High = qlnorm(test.output.Q.High, meanlog=test_fitted_mean, sdlog=test_fitted_sd)
+
+plot(dat$predictor,dat$output)
+points(dat$predictor,test.output.Low,col="orange")
+points(dat$predictor,test.output.Mid,col="red")
+points(dat$predictor,test.output.High,col="cyan")
+
+
+res <- list(anomalies_temperature = anomalies$temperature,
+            anomalies_intensity = anomalies$intensity,
+            copulaType = "clayton",
+            quantileLow=test.output.Low,
+            quantileMid=test.output.Mid,
+            quantileHigh=test.output.High
+)
+
+saveRDS(res, file = paste(dataPath,'result2.rds',sep = '/'))
+
+# Step 6 - Count Regression
+poission.model <- glm(intensity ~ temperature, family=poisson(link='log'), data=part2_data)
+summary(poission.model)$deviance
+# 272.786, 248
+
+nb.model <- MASS::glm.nb(intensity ~ temperature, data=part2_data)
+summary(nb.model)$deviance
+# 272.761, 248, 97524
