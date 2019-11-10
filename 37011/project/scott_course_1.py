5d0fbab433d52aa03ab2d1379507db1983fbc252
Scott Shepard <scott@shepardmail.com>
2019-11-10 09:37:53 +0300

    hardcoded full course

--- 37011/project/scott_course_1.py
+++ 37011/project/scott_course_1.py
@@ -22,14 +22,108 @@ class Camera:
             imagename = self.save_path + 'image_{0}_{1}_{2}.jpeg'.format(imageinx, left_speed, right_speed)
             self.camera.capture(imagename)
 
-robot = EasyGoPiGo3()
+class Robot:
+    
+    def __init__(self):
+        self.robot = EasyGoPiGo3()
+    
+    def set_motor(self, turn_ratio, speed_factor):
+        left_speed, right_speed = self.turn_ratio_to_speeds(turn_ratio, speed_factor)
+        self.robot.set_motor_dps(self.robot.MOTOR_LEFT,  left_speed)
+        self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, right_speed)
+    
+    def turn_ratio_to_speeds(self, turn_ratio, speed_factor, max_speed=MAX_SPEED):
+        if turn_ratio == 1:
+            ls, rs =  1.0, 1.0
+        elif turn_ratio < 1:
+            ls, rs = turn_ratio, 1.0
+        elif turn_ratio > 1:
+            ls, rs = 1.0, 1.0/turn_ratio
+        return ls*max_speed*speed_factor, rs*max_speed*speed_factor
+            
+    def stop(self):
+        self.robot.stop()
+
+robot = Robot()
 camera = Camera(RESOLUTION, SAVE_PATH)
 
+def step_01():
+    robot.set_motor(1, 1)
+    sleep(3)
+    robot.stop()
 
-robot.set_motor_dps(robot.MOTOR_RIGHT, 300)
-robot.set_motor_dps(robot.MOTOR_LEFT,  300)
-camera.take_pictures(300, 300)
-time.sleep(2)
-camera.take_pictures(300, 300)
-robot.stop()
+def step_02():
+    robot.set_motor(1.5, .6)
+    sleep(2)
+    robot.stop()
+    
+def step_03():
+    robot.set_motor(1, .85)
+    sleep(4)
+    robot.stop()
+
+def step_04():
+    robot.set_motor(.5, 1)
+    sleep(.8)
+    robot.stop()
+
+def step_05():
+    robot.set_motor(1, 1)
+    sleep(2)
+    robot.stop()
+    
+def step_06():
+    robot.set_motor(.5, .5)
+    sleep(2)
+    robot.stop()
+    
+def step_07():
+    robot.set_motor(1, 1)
+    sleep(2.6)
+    robot.stop()
+    
+def step_08():
+    robot.set_motor(.6, .5)
+    sleep(1.8)
+    robot.stop()
+    
+def step_09():
+    robot.set_motor(1, 1)
+    sleep(1.4)
+    robot.stop()
+    
+def step_10():
+    robot.set_motor(.6, .6)
+    sleep(3)
+    robot.stop()
+    
+def step_11():
+    robot.set_motor(1, 1)
+    sleep(3.5)
+    robot.stop()
+
+def run_course():
+    step_01()
+    step_02()
+    step_03()
+    step_04()
+    step_05()
+    step_06()
+    step_07()
+    step_08()
+    step_09()
+    step_10()
+    step_11()
+    robot.stop()
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
 
